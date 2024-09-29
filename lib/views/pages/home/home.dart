930088bf929baf6bac7db0c7e02c00fc5a0c91b9
S <shakibmohammad18@gmail.com>
2024-09-29 15:30:44 +0600

    upgrade

--- lib/views/pages/home/home.dart
+++ lib/views/pages/home/home.dart
@@ -18,80 +18,84 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "All Shop Products",
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
-          ),
-          backgroundColor: Colors.pink,
-          centerTitle: true,
-          actions: [
-            IconButton(
-                onPressed: (){
-                  Get.to(const ShowCartPage());
-                },
-                icon: const Icon(Icons.shopping_cart,color: Colors.white,size: 35,))
-          ],
+      appBar: AppBar(
+        title: const Text(
+          "All Shop Products",
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
         ),
-        body: Obx(
-          () => controller.isLoading.value
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : GridView.builder(
-                  itemCount: controller.products!.length,
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 0.72,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10),
-                  itemBuilder: (context, index) {
-                    var data = controller.products![index];
-                    return GestureDetector(
-                      onTap: () {
-                        int id = controller.products?[index].productId ?? 0;
-                        Get.to(
-                            ProductDescriptionPage(
-                              productData: data,
-                              id: id,
+        backgroundColor: Colors.pink,
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                Get.to(const ShowCartPage());
+              },
+              icon: const Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+                size: 35,
+              ))
+        ],
+      ),
+      body: Obx(
+        () => controller.isLoading.value
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : GridView.builder(
+                itemCount: controller.products!.length,
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.72,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10),
+                itemBuilder: (context, index) {
+                  var data = controller.products![index];
+                  return GestureDetector(
+                    onTap: () {
+                      int id = controller.products?[index].productId ?? 0;
+                      Get.to(
+                          ProductDescriptionPage(
+                            productData: data,
+                            id: id,
+                          ),
+                          arguments: id);
+                    },
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                              "https://cit-ecommerce-codecanyon.bandhantrade.com/${data.image}"),
+                          Padding(
+                            padding: const EdgeInsets.all(15),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  maxLines: 1,
+                                  data.nameEn.toString(),
+                                  style: const TextStyle(),
+                                ),
+                                Text("Model: ${data.rating ?? ""}"),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "${data.regPrice.toString()} ৳",
+                                  style: const TextStyle(),
+                                ),
+                              ],
                             ),
-                            arguments: id);
-                      },
-                      child: Card(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-
-                            Image.network(
-                                "https://cit-ecommerce-codecanyon.bandhantrade.com/${data.image}"),
-                            Padding(
-                              padding: const EdgeInsets.all(15),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    maxLines: 1,
-                                    data.nameEn.toString(),
-                                    style: const TextStyle(),
-                                  ),
-                                  Text("Model: ${data.rating ?? ""}"),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    "${data.regPrice.toString()} ৳",
-                                    style: const TextStyle(),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
+                          )
+                        ],
                       ),
-                    );
-                  }),
-        ),);
+                    ),
+                  );
+                }),
+      ),
+    );
   }
 }
