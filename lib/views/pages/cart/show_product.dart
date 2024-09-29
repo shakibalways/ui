da374009d51c7748944159c221a840e20ba00091
S <shakibmohammad18@gmail.com>
2024-09-29 15:32:33 +0600

    read and write data

--- lib/views/pages/cart/show_product.dart
+++ lib/views/pages/cart/show_product.dart
@@ -161,7 +161,7 @@ class ShowCartPage extends StatelessWidget {
                                                         Icons.remove))),
                                           ),
                                         ],
-                                      )
+                                      ),
                                     ],
                                   ),
                                 ),
